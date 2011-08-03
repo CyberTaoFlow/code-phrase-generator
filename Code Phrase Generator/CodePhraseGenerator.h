9b3df2049e4a90e61210fbb333714b330ba95ef9
Scott J. Roberts <scott.roberts@gmail.com>
2011-08-03 19:47:29 -0400

    Improved prefix handling and drastically expanded wordlist. No promises on OSX version yet.

---
+++ Code Phrase Generator/CodePhraseGenerator.h
@@ -0,0 +1,17 @@
+//
+//  CodePhraseGenerator.h
+//  Code Phrase Generator
+//
+//  Created by Scott J. Roberts on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CodePhraseGenerator : NSObject {
+@private
+    
+}
+
+@end
---
+++ Code Phrase Generator/CodePhraseGenerator.m
@@ -0,0 +1,29 @@
+//
+//  CodePhraseGenerator.m
+//  Code Phrase Generator
+//
+//  Created by Scott J. Roberts on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CodePhraseGenerator.h"
+
+
+@implementation CodePhraseGenerator
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
